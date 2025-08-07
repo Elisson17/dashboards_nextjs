8bef3df17c0583723b31d95d86f6a7533ae3c99b
Elisson17 <elissoncordeiro2004@gmail.com>
2025-08-07 19:50:13 -0300

    Criando pagina do grafico de barra

--- src/components/Charts/BarCharts/index.tsx
+++ src/components/Charts/BarCharts/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+"use client";
+import Select from "@/components/Select";
+import { SelectedItemBarChart } from "@/utils/selectMap";
+import React from "react";
 
 export default function BarChart() {
+  const [selectedChart, setSelectedChart] = React.useState("TinyBarChart");
+  const selectedItem = SelectedItemBarChart.find(
+    (item) => item.value === selectedChart
+  );
+
   return (
-    <div>BarChart</div>
-  )
+    <div className="flex items-center justify-center flex-col">
+      <Select
+        defaultValue={selectedChart}
+        items={SelectedItemBarChart}
+        placeholder="Selecione um Gráfico"
+        onValueChange={setSelectedChart}
+      />
+      <section className="flex py-10">
+        {selectedItem && selectedItem.content}
+      </section>
+    </div>
+  );
 }
