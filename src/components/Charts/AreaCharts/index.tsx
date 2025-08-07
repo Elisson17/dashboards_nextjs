cf1e30dfce2bcc4b13bca2595af6dc8b4f003952
Elisson17 <elissoncordeiro2004@gmail.com>
2025-08-07 19:50:26 -0300

    Criando pagina do grafico de area

--- src/components/Charts/AreaCharts/index.tsx
+++ src/components/Charts/AreaCharts/index.tsx
@@ -1,5 +1,25 @@
+"use client";
+import Select from "@/components/Select";
+import { SelectedItemAreaChart } from "@/utils/selectMap";
 import React from "react";
 
 export default function AreaChart() {
-  return <div>AreaChart</div>;
+  const [selectedChart, setSelectedChart] = React.useState("SimpleAreaChart");
+  const selectedItem = SelectedItemAreaChart.find(
+    (item) => item.value === selectedChart
+  );
+
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <Select
+        defaultValue={selectedChart}
+        items={SelectedItemAreaChart}
+        placeholder="Selecione um Gráfico"
+        onValueChange={setSelectedChart}
+      />
+      <section className="flex py-10">
+        {selectedItem && selectedItem.content}
+      </section>
+    </div>
+  );
 }
