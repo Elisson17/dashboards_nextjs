7e74404f724acc1f713f6bddca1b72e4365e33c4
Elisson17 <elissoncordeiro2004@gmail.com>
2025-08-10 02:36:54 -0300

    Adicionando mapeamento de graficos de arvore,dispersão,barra radial,radar,tortas e compostos no index de tabs e selects

--- src/utils/selectMap.tsx
+++ src/utils/selectMap.tsx
@@ -4,9 +4,23 @@ import StackedAreaChart from "@/components/Charts/AreaCharts/StackedAreaChart";
 import BrushBarChart from "@/components/Charts/BarCharts/BrushBarChart";
 import SimpleBarChart from "@/components/Charts/BarCharts/SimpleBarChart";
 import TinyBarChart from "@/components/Charts/BarCharts/TinyBarChart";
+import LineBarAreaComposedChart from "@/components/Charts/ComposedChart/LineBarAreaComposedChart";
+import SameDataComposedChart from "@/components/Charts/ComposedChart/SameDataComposedChart";
+import VerticalComposedChart from "@/components/Charts/ComposedChart/VerticalComposedChart";
 import DashedLineChart from "@/components/Charts/LineCharts/DashedLineChart";
 import SimpleLineChart from "@/components/Charts/LineCharts/SimpleLineChart";
 import TinyLineChart from "@/components/Charts/LineCharts/TinyLineChart";
+import StraightAnglePieChart from "@/components/Charts/PieChart/StraightAnglePieChart";
+import TwoLevelPieChart from "@/components/Charts/PieChart/TwoLevelPieChart";
+import TwoSimplePieChart from "@/components/Charts/PieChart/TwoSimplePieChart";
+import SimpleRadarChart from "@/components/Charts/RadarChart/SimpleRadarChart";
+import SpecifiedDomainRadarChart from "@/components/Charts/RadarChart/SpecifiedDomainRadarChart";
+import SimpleRadialBarChart from "@/components/Charts/RadialBarChart/SimpleRadialBarChart";
+import JointLineScatterChart from "@/components/Charts/ScatterChart/JointLineScatterChart";
+import SimpleScatterChart from "@/components/Charts/ScatterChart/SimpleScatterChart";
+import ThreeDimScatterChart from "@/components/Charts/ScatterChart/ThreeDimScatterChart";
+import CustomContentTreemap from "@/components/Charts/TreeMap/CustomContentTreemap";
+import SimpleTreemap from "@/components/Charts/TreeMap/SimpleTreemap";
 import React from "react";
 
 export interface SelectItems {
@@ -66,5 +80,93 @@ export const SelectedItemBarChart: SelectItems[] = [
     value: "BrushBarChart",
     label: "Brush Bar Chart",
     content: <BrushBarChart />,
+  },
+];
+
+export const SelectedItemComposedChart: SelectItems[] = [
+  {
+    value: "LineBarAreaComposedChart",
+    label: "Line Bar Area Composed Chart",
+    content: <LineBarAreaComposedChart />,
+  },
+  {
+    value: "SameDataComposedChart",
+    label: "Same Data Composed Chart",
+    content: <SameDataComposedChart />,
+  },
+  {
+    value: "VerticalComposedChart",
+    label: "Vertical Composed Chart",
+    content: <VerticalComposedChart />,
+  },
+];
+
+export const SelectedItemScatterChart: SelectItems[] = [
+  {
+    value: "SimpleScatterChart",
+    label: "Simple Scatter Chart",
+    content: <SimpleScatterChart />,
+  },
+  {
+    value: "ThreeDimScatterChart",
+    label: "Three Dim Scatter Chart",
+    content: <ThreeDimScatterChart />,
+  },
+  {
+    value: "JointLineScatterChart",
+    label: "Joint Line Scatter Chart",
+    content: <JointLineScatterChart />,
+  },
+];
+
+export const SelectedItemPieChart: SelectItems[] = [
+  {
+    value: "TwoLevelPieChart",
+    label: "Two Level Pie Chart",
+    content: <TwoLevelPieChart />,
+  },
+  {
+    value: "StraightAnglePieChart",
+    label: "Straight Angle Pie Chart",
+    content: <StraightAnglePieChart />,
+  },
+  {
+    value: "TwoSimplePieChart",
+    label: "Two Simple Pie Chart",
+    content: <TwoSimplePieChart />,
+  },
+];
+
+export const SelectedItemRadarChart: SelectItems[] = [
+  {
+    value: "SimpleRadarChart",
+    label: "Simple Radar Chart",
+    content: <SimpleRadarChart />,
+  },
+  {
+    value: "SpecifiedDomainRadarChart",
+    label: "Specified Domain Radar Chart",
+    content: <SpecifiedDomainRadarChart />,
+  },
+];
+
+export const SelectedItemRadialBarChart: SelectItems[] = [
+  {
+    value: "SimpleRadialBarChart",
+    label: "Simple Radial Bar Chart",
+    content: <SimpleRadialBarChart />,
+  },
+];
+
+export const SelectedItemTreeMap: SelectItems[] = [
+  {
+    value: "SimpleTreeMap",
+    label: "SimpleTreeMap",
+    content: <SimpleTreemap />,
+  },
+  {
+    value: "CustomContentTreemap",
+    label: "CustomContentTreemap",
+    content: <CustomContentTreemap />,
   },
 ];
--- src/utils/tabsMapper.tsx
+++ src/utils/tabsMapper.tsx
@@ -1,6 +1,12 @@
 import AreaChart from "@/components/Charts/AreaCharts";
 import BarChart from "@/components/Charts/BarCharts";
+import ComposedChart from "@/components/Charts/ComposedChart";
 import LineChart from "@/components/Charts/LineCharts";
+import PieChart from "@/components/Charts/PieChart";
+import RadarChart from "@/components/Charts/RadarChart";
+import RadialBarChart from "@/components/Charts/RadialBarChart";
+import ScatterChart from "@/components/Charts/ScatterChart";
+import TreeMap from "@/components/Charts/TreeMap";
 import { TabItem } from "@/components/Tabs";
 
 export const tabsMapperIndex: TabItem[] = [
@@ -18,5 +24,35 @@ export const tabsMapperIndex: TabItem[] = [
     value: "BarChart",
     label: "BarChart",
     content: <BarChart />,
+  },
+  {
+    value: "ComposedChart",
+    label: "ComposedChart",
+    content: <ComposedChart />,
+  },
+  {
+    value: "ScatterChart",
+    label: "ScatterChart",
+    content: <ScatterChart />,
+  },
+  {
+    value: "PieChart",
+    label: "PieChart",
+    content: <PieChart />,
+  },
+  {
+    value: "RadarChart",
+    label: "RadarChart",
+    content: <RadarChart />,
+  },
+  {
+    value: "RadialBarChart",
+    label: "RadialBarChart",
+    content: <RadialBarChart />,
+  },
+  {
+    value: "TreeMap",
+    label: "TreeMap",
+    content: <TreeMap />,
   },
 ];
